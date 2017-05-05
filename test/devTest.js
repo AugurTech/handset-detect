6e80498a49e79248ee405e2c44cff4200833fc72
Nawar Alsafar <nawar@augur.io>
2017-05-05 20:48:54 +0000

    all tests pass

---
+++ test/devTest.js
@@ -0,0 +1,21 @@
+// const handsetDetect = require('../index.js')({
+//     verbose: true,
+//     secret: process.env.HANDSET_DETECT_SECRET,
+//     username: process.env.HANDSET_DETECT_USERNAME,
+//     module: 'hosted',
+//     autoUpdate: true
+// });
+// const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.1 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.1';
+
+// console.time('Lookup');
+// console.log( handsetDetect( userAgent ) );
+// console.timeEnd('Lookup');
+// console.time('Lookup');
+// console.log( handsetDetect( userAgent ) );
+// console.timeEnd('Lookup');
+// console.time('Lookup');
+// console.log( handsetDetect( userAgent ) );
+// console.timeEnd('Lookup');
+
+// console.log( handsetDetect( 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'));
+// process.exit();
